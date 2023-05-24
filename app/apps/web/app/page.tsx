aabe141ed99ca2be17ac09161ec4f014ffce7c06
Turbobot <turbobot@vercel.com>
2023-05-24 13:33:54 -0400

    feat(create-turbo): apply official-starter transform

--- app/apps/web/app/page.tsx
+++ app/apps/web/app/page.tsx
@@ -1,10 +1,12 @@
-import { Button, Header } from "ui";
+import { prisma } from "database";
 
-export default function Page() {
+export default async function IndexPage() {
+  const users = await prisma.user.findMany();
+
   return (
-    <>
-      <Header text="Web" />
-      <Button />
-    </>
+    <div>
+      <h1>Hello World</h1>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+    </div>
   );
 }
