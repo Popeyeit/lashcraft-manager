f3121e561f147cdd84ed1aaf98c3e57947bed3b2
Popeyeit <igorserov28@gmail.com>
2023-08-07 15:58:51 -0400

    updated

--- entities/Artist/ui/CreateArtist/CreateArtist.tsx
+++ entities/Artist/ui/CreateArtist/CreateArtist.tsx
@@ -21,14 +21,16 @@ export const CreateArtist = ({
   const [user] = useUser((state) => [state.user]);
   return (
     <>
-      {isAdminShowed && user?.role === "ADMIN" && ""}
-      <button
-        type="button"
-        onClick={handleOpenModal}
-        className="hover:scale-110 ease-linear duration-300 fixed bottom-[120px] right-10 rounded-[50%] bg-primary w-[60px] h-[60px] text-[34px] leading-[60px] text-white"
-      >
-        +
-      </button>
+      {isAdminShowed && user?.role === "ADMIN" && (
+        <button
+          type="button"
+          onClick={handleOpenModal}
+          className="hover:scale-110 ease-linear duration-300 fixed bottom-[120px] right-10 rounded-[50%] bg-primary w-[60px] h-[60px] text-[34px] leading-[60px] text-white"
+        >
+          +
+        </button>
+      )}
+
       {isOpen && <Modal onCloseModal={handleCloseModal}>{children}</Modal>}
     </>
   );
